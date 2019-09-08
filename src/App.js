f11bf4ce10d1c6dfe0e91c8d736431c14affd755
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-08 13:18:23 +0300

    making the homepage to navigation with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
 
+const HatsPage = ()=>{
+  return(<h1>Hats Page</h1>);
+} 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
