141f69f5aca527f60b6a8322e8356b9244966406
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-10 15:17:34 +0300

    sign in component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage  from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
