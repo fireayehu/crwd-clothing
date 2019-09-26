861acfe64c7b76afee0307b31e14402d8983086f
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-26 17:12:20 +0300

    custom button component

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({children,...otherProps}) => (
+    <button {...otherProps} className="custom-button">
+        {children}
+    </button>
+);
+
+export default CustomButton;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -40,7 +41,9 @@ class SignIn extends React.Component{
                         label = 'Password'
                         required
                     />
-                    <input type="submit" value="Submit Form"/>
+                    <CustomButton type="submit">
+                        Sign in
+                    </CustomButton>
                 </form>
             </div>
         );
