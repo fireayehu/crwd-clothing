b2cc272557e16b0fa73cc5549739cee94e9b2a3f
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-27 15:51:55 +0300

    modifiying the custom button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,15 +3,30 @@ import React from 'react';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {Link} from 'react-router-dom';
+import {auth} from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <header className="header">
         <Link to="/" className="logo-container">
             <Logo className="logo"/>
         </Link>
         <nav className="options">
-            <Link to="/shop" className="option">SHOP</Link>
-            <Link to="/shop" className="option">CONTACT</Link>
+            <Link to="/shop" className="option">
+                SHOP
+            </Link>
+            <Link to="/shop" className="option">
+                CONTACT
+            </Link>
+            {
+                currentUser?
+                    <div className="option" onClick={_=> auth.signOut()}>
+                        SIGN OUT
+                    </div>
+                :
+                    <Link to="/signin" className="option">
+                        SIGN IN
+                    </Link>
+            }
         </nav>
     </header>
 );
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -42,12 +42,14 @@ class SignIn extends React.Component{
                         label = 'Password'
                         required
                     />
-                    <CustomButton type="submit">
-                        Sign in
-                    </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        Sign in with Google
-                    </CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">
+                            Sign in
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         );
