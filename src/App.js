2a5bdaf127993292d3a9f741f49c24f91e9e863c
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-28 13:16:41 +0300

    Stoing user data on firestore

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage  from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
@@ -17,9 +17,20 @@ class App extends React.Component {
     this.unsubscribeFromAuth = null;
   }
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }else{
+        this.setState({currentUser:userAuth});
+      }
     });
   }
   componentWillUnmount(){
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,29 @@ const config = {
     appId: "1:784438540836:web:2b7c41a4c60f23b760f6b1",
     measurementId: "G-HLXCJDTCW1"
 };
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+    if(!userAuth) return;
 
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName,email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        }catch(error){
+            console.log('Error Creating User',error.message);
+        }
+        
+    }
+    return userRef;
+}
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
