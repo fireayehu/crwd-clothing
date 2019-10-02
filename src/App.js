6a4c2b6d147500c297d9b687c772a79729abc6cf
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-02 13:57:29 +0300

    user selector and createStructuredSelector

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage  from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -6,7 +8,7 @@ import Header from './components/header/header.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import {Switch,Route,Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
+import {selectCurrentUser} from './redux/user/user.selectors';
 import './App.css';
 
 class App extends React.Component {
@@ -51,8 +53,8 @@ class App extends React.Component {
   }
   
 }
-const mapStateToProps = ({user}) =>({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {Link} from 'react-router-dom';
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {selectCurrentUser} from '../../redux/user/user.selectors';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
 
 
 const Header = ({currentUser,hidden}) => (
@@ -35,8 +38,8 @@ const Header = ({currentUser,hidden}) => (
         {hidden? null : <CartDropdown/>}
     </header>
 );
-const mapStateToProps = ({user:{currentUser},cart:{hidden}}) =>({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 export default connect(mapStateToProps)(Header);
