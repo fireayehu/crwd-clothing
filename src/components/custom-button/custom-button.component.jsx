d233392ce2257d7862ab21f55e06cb80326ffd1d
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-02 10:20:02 +0300

    inveted custom button style to make add to cart button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children,isGoogleSignIn,...otherProps}) => (
-    <button {...otherProps} className={`${isGoogleSignIn ? 'google-sign-in':''} custom-button`}>
+const CustomButton = ({children,isGoogleSignIn,inverted,...otherProps}) => (
+    <button {...otherProps} className={`${inverted ? 'inverted':''} ${isGoogleSignIn ? 'google-sign-in':''} custom-button`}>
         {children}
     </button>
 );
