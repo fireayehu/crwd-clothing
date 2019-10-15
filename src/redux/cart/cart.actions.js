859a5689d05a4d80c71a0796d15ec7ea572933dd
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-15 12:26:28 +0300

    clear cart using saga during userlogout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,18 +1,22 @@
-import {CartActionTypes} from './cart.types';
-export const toggleCartHidden = _ =>({
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
+import { CartActionTypes } from "./cart.types";
+export const toggleCartHidden = _ => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
-export const addItem = (item) =>({
-    type: CartActionTypes.ADD_ITEM,
-    payload: item
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
 });
 
-export const removeItem = (item) =>({
-    type: CartActionTypes.REMOVE_ITEM,
-    payload: item
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
 });
-export const clearItem = (item) =>({
-    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
-    payload: item
+export const clearItem = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
+
+export const clearCart = () => ({
+  type: CartActionTypes.CLEAR_CART
 });
